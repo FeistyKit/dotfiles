b4f355d89f5d0527fba82dad159f5bf7a3273d21
FeistyKit <eeveebeevee33@gmail.com>
2022-01-31 07:32:33 -0500

    doom emacs config

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -56,7 +56,7 @@
 (setq custom-font-size (cond ((file-exists-p "~/.config/doom/local-config") (string-to-number (trim-string ( nth 0 (read-lines "~/.config/doom/local-config"))))) (t 20)))
   ;; reading the size from a file so I can have different local configs for different computers
 
-(setq doom-font (font-spec :family "mononoki Nerd Font" :size custom-font-size))
+(setq doom-font (font-spec :family "Iosevka Term Bold" :size custom-font-size))
 
 (setq racer-rust-src-path
       (concat (string-trim
