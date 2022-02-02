35ea0188b057d7d172acbf43b60b6332c8f3ed4a
FeistyKit <eeveebeevee33@gmail.com>
2022-02-02 09:37:40 -0500

    Elcord and lsp documentation

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -63,6 +63,12 @@
                (shell-command-to-string "rustc --print sysroot"))
               "/lib/rustlib/src/rust/library"))
 
+
+(set-lookup-handlers! 'lsp-mode :documentation nil)
+(set-lookup-handlers! 'lsp-mode :documentation 'lsp-ui-doc-show)
+
+(elcord-mode)
+
 (require 'lsp-pyright)
 
 (ido-mode 1)
--- doom-emacs/packages.el
+++ doom-emacs/packages.el
@@ -32,6 +32,8 @@
 
 (package! ido-completing-read+)
 
+(package! elcord)
+
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
