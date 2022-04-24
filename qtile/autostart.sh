45194d248e0637781b8e976778c6379f29d8bfa8
FeistyKit <eeveebeevee33@gmail.com>
2022-04-24 07:33:49 -0400

    [qtile] improve startup time

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 rm "$HOME"/.moc/pid
 dunst &
-mocp -S
+mocp -S &
 picom --corner-radius 30 &
 setxkbmap -option compose:ralt
 nitrogen --restore &
-emacs --daemon
+emacs --daemon &
 sxhkd -c "$HOME/.config/sxhkd/sxhkdrc" &
 ../utils/xborder/xborders --border-rgba "#FABD2FFF" --border-radius 30 &
