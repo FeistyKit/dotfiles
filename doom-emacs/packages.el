bb8b6c08852c23d5790176553f03683418a43a86
FeistyKit <eeveebeevee33@gmail.com>
2022-05-15 15:30:19 -0400

    [emacs] install gruber-darker-theme

--- doom-emacs/packages.el
+++ doom-emacs/packages.el
@@ -42,6 +42,8 @@
 
 (package! catppuccin-theme)
 
+(package! gruber-darker-theme)
+
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
