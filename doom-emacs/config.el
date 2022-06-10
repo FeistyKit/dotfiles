251dc46e51eac692dd0ef5cb2ad88056dca64279
FeistyKit <eeveebeevee33@gmail.com>
2022-06-10 16:03:29 -0400

    [doom-emacs] add keybinds for indent-region, align-regexp

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -1,9 +1,3 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "FeistyKit"
@@ -65,23 +59,6 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
 
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
 (use-package! tree-sitter
   :config
   (require 'tree-sitter-langs)
@@ -93,3 +70,11 @@
 ;; Set transparency: taken from https://askubuntu.com/questions/1007434/how-to-make-emacs-transparent-with-i3-wm
 ;; (set-frame-parameter (selected-frame) 'alpha '(90 . 90))
 ;; (add-to-list 'default-frame-alist '(alpha . (90 . 90)))
+
+;; add indent-region functionality
+(map! :leader
+      :desc "Fix indentation of region" "c i" #'indent-region)
+
+;; add indent-region functionality
+(map! :leader
+       :desc "Align by regex" "c b" #'align-regexp)
