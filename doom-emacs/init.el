9087799ae9f0fd77c1d0642089e9bf1704d3e7bd
FeistyKit <eeveebeevee33@gmail.com>
2022-06-10 16:03:16 -0400

    [doom-emacs] clean up languages

--- doom-emacs/init.el
+++ doom-emacs/init.el
@@ -56,7 +56,7 @@
        (evil +everywhere)             ; come to the dark side, we have cookies
        file-templates                 ; auto-snippets for empty files
        fold                           ; (nigh) universal code folding
-       format                         ; automated prettiness
+       ;;format                         ; automated prettiness
        ;;god                             ; run Emacs commands without modifier keys
        ;;lispy                        ; vim for lisp, for people who don't like vim
        multiple-cursors               ; editing in many places at once
@@ -114,11 +114,11 @@
        ;;agda                         ; types of types of types of types...
        ;;beancount                    ; mind the GAAP
        (cc +lsp)                      ; C > C++ == 1
-       (clojure +lsp)                 ; java with a lisp
-       (common-lisp +lsp)             ; if you've seen one lisp, you've seen them all
+       ;;(clojure +lsp)                 ; java with a lisp
+       ;;(common-lisp +lsp)             ; if you've seen one lisp, you've seen them all
        ;;coq                          ; proofs-as-programs
        ;;crystal                      ; ruby at the speed of c
-       (csharp +lsp)                       ; unity, .NET, and mono shenanigans
+       ;;(csharp +lsp)                       ; unity, .NET, and mono shenanigans
        ;;data                         ; config/data formats
        ;;(dart +flutter)              ; paint ui and not much else
        ;;dhall
@@ -131,14 +131,14 @@
        ;;faust                        ; dsp, but you get to keep your soul
        ;;fsharp                       ; ML stands for Microsoft's Language
        ;;fstar                        ; (dependent) types and (monadic) effects and Z3
-       (gdscript +lsp)                ; the language you waited for
-       (go +lsp)                      ; the hipster dialect
-       (haskell +lsp)                 ; a language that's lazier than I am
+       ;;(gdscript +lsp)                ; the language you waited for
+       ;;(go +lsp)                      ; the hipster dialect
+       ;;(haskell +lsp)                 ; a language that's lazier than I am
        ;;hy                           ; readability of scheme w/ speed of python
        ;;idris                        ; a language you can depend on
        ;;json                         ; At least it ain't XML
        ;;(java +meghanada)            ; the poster child for carpal tunnel syndrome
-       (javascript +lsp)              ; all(hope(abandon(ye(who(enter(here))))))
+       ;;(javascript +lsp)              ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia                        ; a better, faster MATLAB
        ;;kotlin                       ; a better, slicker Java(Script)
        ;;latex                        ; writing papers in Emacs has never been so fun
